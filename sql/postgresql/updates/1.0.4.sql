21111df7e0d380b7aaec123b468ef513231be357
zero-24 <zero-24@users.noreply.github.com>
2026-05-30 19:24:14 +0200

    add extensionid and bump version

---
+++ sql/postgresql/updates/1.0.4.sql
@@ -0,0 +1,4 @@
+-- Update existing mail template and add the extensionid param
+UPDATE "#__mail_templates"
+SET "params" = '{"tags": ["newversion", "curversion", "sitename", "url", "updatelink", "extensiontype", "extensionname", "extensionid"]}'
+WHERE "template_id" = 'plg_task_extensionupdates.extension_update' AND  "extension" = 'plg_task_extensionupdates';
